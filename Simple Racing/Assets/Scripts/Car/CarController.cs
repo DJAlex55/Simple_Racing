52b98ee09babe6af25c08e3c09e9ca2f225b1c38
DJAlex55 <djalexy555@gmail.com>
2016-10-01 14:09:10 +0200

    ho uploadato tutto ciò che ho fatto fin ora
    tutto il progetto unity3d

---
+++ Simple Racing/Assets/Scripts/Car/CarController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    [SerializeField]
+    private float YOffset;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    private List<AxleInfo> axleInfos; // the information about each individual axle
+    [SerializeField]
+    private float maxMotorTorque; // maximum torque the motor can apply to wheel
+    [SerializeField]
+    private float maxSteeringAngle; // maximum steer angle the wheel can have
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass -= new Vector3 (0, YOffset, 0);
+    }
+
+
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");   
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+
+
+}
+
+
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
---
+++ Simple Racing/Assets/Scripts/Car/WheelScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelScript : MonoBehaviour {
+
+    [SerializeField]
+    private WheelCollider wheelCollider;
+
+    Vector3 pos;
+    Quaternion rot;
+
+    void Start()
+    {
+        pos = transform.position;
+        rot = transform.rotation;
+
+        if (pos == null || rot == null)
+            Debug.LogError("position and/or rotation are null");
+    }    
+
+	void FixedUpdate()
+    {
+        wheelCollider.GetWorldPose(out pos, out rot);
+        transform.position = pos;
+        transform.rotation = rot;
+    }
+}
