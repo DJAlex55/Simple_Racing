b5411b1dab8f4834cfe961ba98da008e41227d37
DJAlex55 <djalexy555@gmail.com>
2016-10-13 22:01:59 +0200

    updated with more relistic car control

--- Simple Racing/Assets/Scripts/Car/CarController.cs
+++ Simple Racing/Assets/Scripts/Car/CarController.cs
@@ -3,37 +3,53 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody))]
 public class CarController : MonoBehaviour
 {
+    [Header("Car setUp")]
     [SerializeField]
-    private Transform centerOfMass;
+    private Transform centerOfMass;//use this to artificially modify the rigidBody CenterOfMass
 
-    bool stabilizer = true;
+    bool stabilizer = true;//does the car uses 
+
+    
+    [SerializeField]
+    private List<AxleInfo> axleInfos;//list of every axle the car has
+
+    [Header("Car stats")]
+    [SerializeField]
+    private float maxMotorTorque;//how much can the wheel motor torque be
 
     [SerializeField]
-    private List<AxleInfo> axleInfos; // the information about each individual axle
+    private float maxSteeringAngle;//how much can the wheels steer
+
     [SerializeField]
-    private float maxMotorTorque; // maximum torque the motor can apply to wheel
+    private float maxHighBrakeTorque;//how much can the wheel brake torque be
+
     [SerializeField]
-    private float maxSteeringAngle; // maximum steer angle the wheel can have
+    private float maxLowBrakeTorque;//how much can the wheel brake torque be
 
     [SerializeField]
     private Light[] FrontLights;
+
     [SerializeField]
     private Light[] BackLights;
+
     [SerializeField]
     private float maxLightIntensity;
 
-    private float currentSpeed;
-
-    float wheelCircumference;
+    Rigidbody rb;
+    private float speed;
+    public float Speed
+    {
+        get { return speed; }
+    }
+        
 
     void Awake()
     {
         Vector3 com = GetComponent<Rigidbody>().centerOfMass;
         com.y = centerOfMass.position.y;
+        com.z = centerOfMass.position.z;
 
-        wheelCircumference = 2 * Mathf.PI * axleInfos[0].leftWheel.radius;
-
-        Rigidbody rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
 
         foreach (AxleInfo axleInfo in axleInfos)
         {
@@ -43,38 +59,65 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        Vector3 localspeed = transform.InverseTransformDirection(rb.velocity);
 
-        currentSpeed = 0f;
+        float v = Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");        
+        float brake = maxHighBrakeTorque * Input.GetAxis("Brake");
+        float motor = maxMotorTorque * v;
+        float lowBrake = maxLowBrakeTorque * v;        
+
         foreach (AxleInfo axleInfo in axleInfos)
         {
+            setMotorTorque(axleInfo, 0f);
+            setBrakeTorque(axleInfo, 0f);
 
             if (axleInfo.steering)
-            {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
+                setSteeringAngle(axleInfo, steering);
 
-            }
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-            }
-            else
-            {
-                currentSpeed = ((axleInfo.rightWheel.rpm + axleInfo.leftWheel.rpm) * wheelCircumference) / 2f;
+                if (motor >= 0f)
+                    setMotorTorque(axleInfo, motor);
+                else
+                {   
+                    if (localspeed.z <= 0.5f)
+                        setMotorTorque(axleInfo, motor);
+                    else
+                    {
+
+                        setMotorTorque(axleInfo, 0f);
+                        setBrakeTorque(axleInfo, lowBrake);
+                    }
+                }
             }
 
-            if(stabilizer)
-                axleInfo.Stable();
+            if (localspeed.z > 0.5f)
+            {
+
+                setMotorTorque(axleInfo, 0f);
+                setBrakeTorque(axleInfo, lowBrake);
+            }
+
+            if (axleInfo.brake)
+            {
+                //if (brake != 0f)
+                //{
+                //    axleInfo.leftWheel.motorTorque = 0f;
+                //    axleInfo.rightWheel.motorTorque = 0f;
+                //}
+
+                setBrakeTorque(axleInfo, brake);
+            }
+
+            axleInfo.Stable();
         }
 
         if (motor < 0f)
         {
             foreach (Light light in BackLights)
             {
-                light.intensity = ((maxLightIntensity - 1) * Mathf.Abs(Input.GetAxis("Vertical")) + 1);
+                light.intensity = ((maxLightIntensity - 1) * Mathf.Abs(Input.GetAxis("Vertical"))) + 1;
             }
 
         }
@@ -86,24 +129,34 @@ public class CarController : MonoBehaviour
             }
         }
 
-        GetSpeed();
+        speed = GetSpeed();
 
     }
 
-    float GetSpeed()
+
+    public float GetSpeed()
     {
-        return currentSpeed * 60f / 1000f;
+        return rb.velocity.magnitude;
     }
 
 
-    void Update()
+    private void setMotorTorque(AxleInfo axle, float value)
     {
-        if(Input.GetKeyDown(KeyCode.N))
-        {
-            stabilizer = !stabilizer;
-        }
+        axle.leftWheel.motorTorque = value;
+        axle.rightWheel.motorTorque = value;
     }
 
+    private void setBrakeTorque(AxleInfo axle, float value)
+    {
+        axle.leftWheel.brakeTorque = value;
+        axle.rightWheel.brakeTorque = value;
+    }
+
+    private void setSteeringAngle(AxleInfo axle, float angle)
+    {
+        axle.leftWheel.steerAngle = angle;
+        axle.rightWheel.steerAngle = angle;
+    }
 
 }
 
@@ -115,6 +168,7 @@ public class AxleInfo
     public WheelCollider rightWheel;
     public bool motor;
     public bool steering;
+    public bool brake;
     public float AntiRoll = 25000f;
     [HideInInspector]
     public Rigidbody rb;
---
+++ Simple Racing/Assets/Scripts/SpeedUIScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeedUIScript : MonoBehaviour
+{
+    public string playerTag;
+
+    private Text UIText;
+
+    CarController Car;
+
+    void Awake()
+    {
+        Car = GameObject.FindGameObjectWithTag(playerTag).GetComponent<CarController>();
+        UIText = GetComponent<Text>();
+        StartCoroutine("UpdateText");
+    }
+
+    private IEnumerator UpdateText()
+    {
+        while (true)
+        {
+            UIText.text = ((int)Car.Speed).ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
